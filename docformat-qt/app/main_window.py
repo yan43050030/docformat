53f234fd1b20bee37537f8e2f83b9c332490b4ad
yan43050030 <yan.whu@gmail.com>
2026-07-21 12:09:47 +0800

    v2.6.5: widow_control + east_asian_typography

--- docformat-qt/app/main_window.py
+++ docformat-qt/app/main_window.py
@@ -14,7 +14,7 @@ from app.pages.log_page import LogPage
 from app.pages.template_draft_page import TemplateDraftPage
 from app.pages.template_maker_page import TemplateMakerPage
 
-VERSION = '2.6.4'
+VERSION = '2.6.5'
 NAV_ITEMS = [('格式处理', 0), ('版式方案', 1), ('文书起草', 2), ('文书模板制作', 3),
              ('主题', 4), ('日志', 5)]
 
